576374f9aa84505f4ab2fb9e7c72dd8b48e50099
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-01-22 17:11:14 +0300

    find duplicates in directories

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,26 @@
+import os
+import itertools
+import filecmp
+import sys
 
+def are_files_duplicates(file_path1,file_path2):
+    difference_files = filecmp.cmp(file_path1,file_path2)
+    return  difference_files    
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
-
+def get_files(root):
+    files_list = []
+    for subdir, dirs, files in os.walk(root):
+        for file in files:
+            files_list.append(os.path.join(subdir, file))
+    return files_list        
 
 if __name__ == '__main__':
-    pass
+    files =  []
+    root = sys.argv[1]
+    files = get_files(root) 
+    for file1, file2 in itertools.combinations(files,2):
+        if (are_files_duplicates(file1,file2)):
+            print("Файл {} есть в папке {}".format(file1,file2))
+
+    
+
